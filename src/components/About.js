24cad18133dd3312d19a068a34598c13ce3e92b7
symplyijay <ijeonyi@gmail.com>
2022-05-07 22:14:31 +0100

    portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -14,16 +14,15 @@ const About = () => {
 						<div className='col-md-6 text-white '>
 							<h3 className='fs-2 about-text'>About me</h3>
 							<p className='lh-lg slide'>
-								Hi, I'm Ijeoma, a self-taught front-end developer... supportive,
-								reliable, your information is secured, patient, and observant. I
-								come from a non-tech background but am passionate about web
-								development (frontend) and want to build a career in tech, am
-								going through online courses to improve myself and grow
-								passionate about leveraging my skills to give users an amazing
-								client-side experience. My collaborative skills have helped me
-								deliver well in my projects. Being diligent, I'm open to
-								contract jobs and collaboration projects. You can always reach
-								out to me.
+								Hi, I'm Ijeoma, a front-end developer... supportive, reliable,
+								your information is secured, patient, and observant. I come from
+								a non-tech background but am passionate about web development
+								(frontend) and want to build a career in tech, am going through
+								online courses to improve myself and grow passionate about
+								leveraging my skills to give users an amazing client-side
+								experience. My collaborative skills have helped me deliver well
+								in my projects. Being diligent, I'm open to contract jobs and
+								collaboration projects. You can always reach out to me.
 							</p>
 						</div>
 						<div className='col-md-6 about-img'>
@@ -34,7 +33,7 @@ const About = () => {
 							/>
 						</div>
 						<a
-							href='https://drive.google.com/file/d/1XeqwOzXt5hW_dGuzKBHkrSbAxgVnrdZp/view?usp=sharing'
+							href='https://drive.google.com/file/d/1zwAabE7gxiVPNfgGyGcZhfCyAWGvWRGS/view?usp=sharing'
 							target=' _blank'>
 							<button
 								className='border-0 p-3 rounded-2 mb-4 hero-hover'
