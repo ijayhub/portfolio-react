6f196d5bcf4fe92bbf06c1405b2943a839ab3a12
symplyijay <ijeonyi@gmail.com>
2022-06-09 10:30:55 +0100

    portfolio

--- src/components/Works.js
+++ src/components/Works.js
@@ -2,6 +2,9 @@ import React from 'react'
 import mealGenerator from '../img/mealGenerator.jpeg'
 import travelTour from '../img/travelTour.png'
 import galleryClick from '../img/gallery-click.png'
+import News from '../img/news.png'
+import book from '../img/bookapp.png'
+import Note from '../img/note-app.png'
 
 
 
@@ -9,11 +12,16 @@ import galleryClick from '../img/gallery-click.png'
 const Works = () => {
 	const travel = 'https://fun-travel.vercel.app/'
 	const travelGit = 'https://github.com/ijayhub/fun-travel';
-	
 	const gallery = 'https://ijayhub.github.io/gallery-click/';
 	const galleryGit = 'https://github.com/ijayhub/gallery-click';
 	const meal = 'https://meal-webapp.netlify.app';
 	const mealGit = 'https://github.com/ijayhub/meal-app';
+	const news = 'https://news-nytimes.netlify.app';
+	const newsGit = 'https://github.com/ijayhub/news-articles';
+	const Book = 'https://book-app1.netlify.app';
+	const BookGit = 'https://github.com/ijayhub/book-app';
+	const note = 'https://note-web-app.netlify.app';
+	const noteGit = 'https://github.com/ijayhub/note-app';
 
     return (
 			<div className='bg-light' id='portfolio'>
@@ -86,6 +94,76 @@ const Works = () => {
 								</a>
 								<a
 									href={mealGit}
+									target='_blank'
+									rel='noopener noreferrer'
+									className='text-dark'>
+									Link to the GitHub codes
+								</a>
+							</p>
+						</div>
+						<div class='col-md-4 slide-in-right '>
+							<img
+								src={News}
+								alt={News}
+								style={{ width: '300px', height: '170px', objectFit: 'cover' }}
+							/>
+							<p>
+								<a
+									href={news}
+									target='_blank'
+									rel='noopener noreferrer'
+									className='text-dark mx-2'>
+									Live Link to the work
+								</a>
+								<a
+									href={newsGit}
+									target='_blank'
+									rel='noopener noreferrer'
+									className='text-dark'>
+									Link to the GitHub codes
+								</a>
+							</p>
+						</div>
+						<div class='col-md-4 slide-in-bottom'>
+							<img
+								src={book}
+								alt={book}
+								style={{ width: '300px', height: '170px', objectFit: 'cover' }}
+							/>
+
+							<p>
+								<a
+									href={Book}
+									target='_blank'
+									rel='noopener noreferrer'
+									className='text-dark px-3'>
+									Live Link to the work
+								</a>
+								<a
+									href={BookGit}
+									target='_blank'
+									rel='noopener noreferrer'
+									className='text-dark'>
+									Link to the GitHub codes
+								</a>
+							</p>
+						</div>
+						<div class='col-md-4 slide-in-left'>
+							<img
+								src={Note}
+								alt={Note}
+								style={{ width: '300px', height: '170px', objectFit: 'cover' }}
+							/>
+							<p>
+								<a
+									href={note}
+									target='_blank'
+									rel='noopener noreferrer'
+									className='px-3 text-dark'>
+									Live Link to the work
+								</a>
+								<a
+									href={noteGit}
 									target='_blank'
 									rel='noopener noreferrer'
 									className='text-dark'>
